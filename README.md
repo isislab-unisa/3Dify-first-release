c568e113d322eb9a8d5a86e4ae94f47a1c2f2357
Daniele <daniele@picaresquestudio.com>
2023-10-24 11:24:45 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 3Dify
 Automatic avatar generator from 2D image
+
+Acknowledgement to the repo https://github.com/justadudewhohacks/face-api.js
---
+++ makeHuman_Exporter/public_html/public/js/imageSelectionControls.js
@@ -0,0 +1,50 @@
+async function onSelectedImageChanged(uri) {
+  const img = await faceapi.fetchImage(uri)
+  $(`#inputImg`).get(0).src = img.src
+  updateResults()
+}
+
+async function loadImageFromUrl(url) {
+  const img = await requestExternalImage($('#imgUrlInput').val())
+  $('#inputImg').get(0).src = img.src
+  updateResults()
+}
+
+async function loadImageFromUpload() {
+    const imgFile = $('#queryImgUploadInput').get(0).files[0]
+    const img = await faceapi.bufferToImage(imgFile)
+    $('#inputImg').get(0).src = img.src
+    updateResults()
+}
+
+function renderImageSelectList(selectListId, onChange, initialValue) {
+  let images = ['therock.png']
+
+  function renderChildren(select) {
+    images.forEach(imageName =>
+      renderOption(
+        select,
+        imageName,
+        imageName
+      )
+    )
+  }
+
+  renderSelectList(
+    selectListId,
+    onChange,
+    initialValue,
+    renderChildren
+  )
+}
+
+function initImageSelectionControls(initialValue = 'therock.png') {
+  renderImageSelectList(
+    '#selectList',
+    async (uri) => {
+      await onSelectedImageChanged(uri)
+    },
+    initialValue
+  )
+  onSelectedImageChanged($('#selectList select').val())
+}
